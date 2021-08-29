c2668634675d88f173696d7485f053a920098d16
Robert Odrowaz <robert@odrowaz.dev>
2021-08-30 00:00:19 +0200

    Add StreamSubscriptionDisposed extension method

---
+++ packages/dispose_scope/test/disposed_extensions/stream_subscription_disposed_test.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:dispose_scope/src/dispose_scope.dart';
+import 'package:dispose_scope/src/disposed_extensions/stream_subscription_disposed.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'stream_subscription_disposed_test.mocks.dart';
+
+@GenerateMocks([StreamSubscription])
+void main() {
+  group(
+    'StreamSubscriptionDisposed',
+    () {
+      late DisposeScope scope;
+      // ignore: cancel_subscriptions
+      late MockStreamSubscription streamSubscription;
+
+      setUp(
+        () {
+          scope = DisposeScope();
+          streamSubscription = MockStreamSubscription();
+        },
+      );
+
+      test(
+        'adds Dispose to DisposeScope when disposed is called',
+        () async {
+          streamSubscription.disposed(scope);
+
+          await scope.dispose();
+
+          verify(streamSubscription.cancel()).called(1);
+        },
+      );
+    },
+  );
+}
