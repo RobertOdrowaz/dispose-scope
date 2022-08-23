af19ca76e77e064b33ac8d13365c87021c5535a5
Bartek Pacia <barpac02@gmail.com>
2022-08-24 01:23:47 +0200

    dont use fancy words

--- packages/dispose_scope/lib/src/disposable.dart
+++ packages/dispose_scope/lib/src/disposable.dart
@@ -1,6 +1,6 @@
 import 'package:dispose_scope/dispose_scope.dart';
 
-/// An entity that can be disposed.
+/// An object that can be disposed.
 abstract class Disposable {
   /// Adds this object to [disposeScope].
   void disposed(DisposeScope disposeScope) {
