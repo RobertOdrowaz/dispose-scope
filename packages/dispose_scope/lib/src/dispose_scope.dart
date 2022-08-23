8d3209aa8bae4d5f079d8a8c07ed638d3e5327f8
Bartek Pacia <barpac02@gmail.com>
2022-08-24 01:27:34 +0200

    make `DisposeScope` inherit from `Disposable`

--- packages/dispose_scope/lib/src/dispose_scope.dart
+++ packages/dispose_scope/lib/src/dispose_scope.dart
@@ -1,10 +1,11 @@
+import 'package:dispose_scope/src/disposable.dart';
 import 'package:dispose_scope/src/dispose.dart';
 
 /// Groups many [DisposeCallback]s together and allows for disposing of all of
 /// them at once.
 ///
 /// [DisposeCallback]s are disposed synchronously in the LIFO order.
-class DisposeScope {
+class DisposeScope extends Disposable {
   final _disposes = <DisposeCallback>[];
 
   /// Adds this [disposeCallback] to this [DisposeScope]'s internal list of
@@ -16,6 +17,7 @@ class DisposeScope {
   }
 
   /// Disposes all added [DisposeCallback]s.
+  @override
   Future<void> dispose() async {
     for (final dispose in _disposes.reversed) {
       await dispose();
