af3c278da6e21e8d487954e4112deb2baa5508af
Bartek Pacia <barpac02@gmail.com>
2022-11-03 13:25:25 +0100

    add annotations to Disposable class

--- packages/dispose_scope/lib/src/disposable.dart
+++ packages/dispose_scope/lib/src/disposable.dart
@@ -1,12 +1,15 @@
 import 'package:dispose_scope/dispose_scope.dart';
+import 'package:meta/meta.dart';
 
 /// An object that can be disposed.
 abstract class Disposable {
   /// Adds this object to [disposeScope].
+  @nonVirtual
   void disposedBy(DisposeScope disposeScope) {
     disposeScope.addDispose(dispose);
   }
 
   /// Disposes this object.
+  @mustCallSuper
   Future<void> dispose();
 }
